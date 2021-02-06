13d090df4120d9019166516961fe23f42c1d0bc1
boosun13 <double0two22@gmail.com>
2021-02-06 15:20:16 +0900

    restart

--- admin-app/src/app/app.module.ts
+++ admin-app/src/app/app.module.ts
@@ -1,35 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-//importして使えるようにする。
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { HttpClientInMemoryWebApiModule } from "angular-in-memory-web-api";
-import { InMemoryDataService } from "./in-memory-data.service";
 
 import { AppComponent } from './app.component';
-import { MembersComponent } from './members/members.component';
-import { MemberDetailComponent } from './member-detail/member-detail.component';
-import { MessagesComponent } from './messages/messages.component';
-import { AppRoutingModule } from './app-routing.module';
-import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MembersComponent,
-    MemberDetailComponent,
-    MessagesComponent,
-    DashboardComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    //ngModuleを使用できるようにする。
-    FormsModule,
-    HttpClientInMemoryWebApiModule.forRoot(
-      InMemoryDataService, { dataEncapsulation: false}
-    ),
-    HttpClientModule,
-    AppRoutingModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- admin-app/src/app/member.ts
+++
@@ -1,7 +0,0 @@
-export interface Member {
-  id: number;
-  name: string;
-}
-// 大文字
-//DBの定義(Rubyと一緒)
-// exportをつけることで他のファイルからimportができる
